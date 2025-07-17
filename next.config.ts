980dbef8667cac1ba41a38d194f9b1d6ae4f5aa2
CryptracSolutions <info@cryptrac.com>
2025-07-16 20:52:15 -0400

    Remove force dynamic on next.config.ts for login to fix prerender env error on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    dynamic: "force-dynamic", // Disable static rendering globally (safe for auth-heavy app)
-  },
+  /* config options here */
 };
 
 export default nextConfig;
