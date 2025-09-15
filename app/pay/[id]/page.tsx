287145c54e465284582e9ef2412dcd8b0bc5081f
CryptracSolutions <info@cryptrac.com>
2025-09-15 15:30:00 -0400

    Dashboard UI/UX Upgrades 2

--- app/pay/[id]/page.tsx
+++ app/pay/[id]/page.tsx
@@ -592,7 +592,6 @@ export default function PaymentPage() {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-[#7f5efd]"></div>
-        </div>
       </div>
     )
   }
